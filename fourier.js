9727a24ef725fabe721d43ffed710085cde9ae47
asteriskman7 <asteriskman@gmail.com>
2019-02-24 12:21:38 -0500

    bring letterPaths.js in and start using it

--- fourier.js
+++ fourier.js
@@ -6,8 +6,11 @@ class Fourier {
     this.ctx = this.canvas.getContext('2d');
     this.canvas.style.display = 'inline';
     this.state = {};
-    this.t = 0;
+    this.angle = 0;
     this.snailSpeed = 1;
+    this.setLevel(2);
+    this.loopTime = 5;
+    this.lastDrawEdges = 0;
   }
   setRelations(parent, child) {
     this.parent = parent;
@@ -27,15 +30,134 @@ class Fourier {
     ctx.save();
 
     ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
-    ctx.fillStyle = 'red';
-    ctx.font = '30 Courier';
-    ctx.fillText('Fourier', 10, 10);
+    //ctx.fillStyle = 'red';
+    //ctx.font = '30 Courier';
+    //ctx.fillText('Fourier', 10, 10);
+    ctx.translate(this.canvas.width * 0.5, this.canvas.height * 0.5);
 
-    ctx.fillRect((this.t*100) % 100, 100, 10, 10);
+    //let cx = this.canvas.width * 0.5;
+    //let cy = this.canvas.height * 0.5;
+    let cx = 0;
+    let cy = 0;
+
+    ctx.fillStyle = 'green';
+    ctx.strokeStyle = 'RGBA(255,255,255,0.3)';
+
+    this.units.forEach( (unit, i) => {
+      //if (i === 0) {return;}
+      const angle = unit.freq * this.angle;
+      const dx = unit.mag * Math.cos(angle + unit.phase);
+      const dy = unit.mag * Math.sin(angle + unit.phase);
+      ctx.beginPath();
+      ctx.arc(cx, cy, unit.mag, 0, Math.PI * 2);
+      ctx.stroke();
+      cx += dx;
+      cy += dy;
+      ctx.beginPath();
+      ctx.arc(cx, cy, 3, 0, Math.PI * 2);
+      ctx.fill();
+    });
+
+    if (this.points.length >= this.units.length) {
+      this.points = [];
+    }
+    this.points.push({x: cx, y: cy});
+    this.parent.feed(1);
+
+    ctx.strokeStyle = 'grey';
+    ctx.beginPath();
+    const points = this.points;
+    ctx.moveTo(points[0].x, points[0].y);
+    for (let i = 1; i < points.length; i++) {
+      ctx.lineTo(points[i].x, points[i].y);
+    }
+    ctx.stroke();
+
+    //test draw a letter
+    /*
+    const lp = letterPoints.i;
+    const s = 25;
+    ctx.beginPath();
+    ctx.moveTo(lp[0][0]*s, lp[0][1]*s);
+    for (let i = 1; i < lp.length; i++) {
+      ctx.lineTo(lp[i][0]*s, lp[i][1]*s);
+    }
+    ctx.stroke();
+    for (let i = 0; i < lp.length; i++) {
+      ctx.fillRect(lp[i][0]*s - 5, lp[i][1]*s - 5, 10, 10);
+    }
+    */
 
     ctx.restore();
   }
   update(timestamp, deltaTime) {
-    this.t += this.snailSpeed * deltaTime / 1000;
+    //this.angle += this.snailSpeed * deltaTime / 1000;
+    this.angle += Math.PI * 2 / this.units.length;
+    //this.angle += Math.PI * 2 / 200;
+  }
+  setLevel(n) {
+    const points = [];
+
+    /*
+    const pointCount = 200;
+    for (let i = 0; i < pointCount; i++) {
+      if ((i % 50) >= 25) {
+        points.push([i, pointCount]);
+      } else {
+        points.push([i, -pointCount]);
+      }
+    }
+    */
+
+    const msg = 'hiaeiiaeh';
+    let dx = -40;
+    for (let j = 0; j < msg.length; j++) {
+      const c = msg[j];
+      const lp = letterPoints[c];
+      const s = 7;
+      for (let i = 0; i < lp.length; i++) {
+        points.push([dx*s + lp[i][0]*s,lp[i][1]*s]);
+      }
+      dx += 10;
+    }
+
+
+
+    const factor = 2;
+    const reducedPoints = points.map( (v,i) => {
+      let sumx = 0;
+      let sumy = 0;
+      for (let j = 0; j < factor; j++) {
+        sumx += points[(i + j) % points.length][0];
+        sumy += points[(i + j) % points.length][1];
+      }
+      return [sumx / factor, sumy / factor];
+    });
+
+    this.units = this.dft(reducedPoints);
+    this.points = [];
+  }
+  dft(data) {
+    let X = [];
+    for (let k = 0; k < data.length; k++) {
+      let Xr = 0;
+      let Xi = 0;
+      for (let n = 0; n < data.length; n++) {
+        const [dr, di] = data[n];
+        const angle = 2 * Math.PI * k * n / data.length;
+        Xr += dr * Math.cos(angle) + di * Math.sin(angle);
+        Xi += di * Math.cos(angle) - dr * Math.sin(angle);
+      }
+
+      const mag = Math.sqrt(Xr * Xr + Xi * Xi) / data.length;
+      const phase = Math.atan2(Xi, Xr);
+      X.push({mag, phase, freq: k});
+    }
+
+    X.sort( (a, b) => {
+      return b.mag - a.mag;
+    });
+
+    return X;
   }
 }
--- letterPaths.js
+++ letterPaths.js
@@ -2,21 +2,21 @@
 
 const letterPaths = {
 a: 'l 2.95293883,-9.33129 1.0866816,0.0236 2.0079985,6.52009 h -5.126302 5.1026784 l 0.8976937,2.76395',
-e: 'v -9.37114 H 5.5458425 0.68487816 v 4.19279 H 5.3955037 0.66817382 v 4.66052 H 5.6126599',
+e: 'v -9.37114 H 5.5458425 0.0 v 4.19279 H 5.3955037 0.0 v 4.66052 H 5.6126599',
 h: 'v -9.83886 4.69392 H 6.1973121 v -4.71062 9.88897',
 i: 'H 2.9065561 v -8.8366 H 0.01670434 5.896634 2.9065561 v 8.80319 h 2.9733734'
 };
 
 const letterPoints = {};
 
-function pathToPoints(path) {
+function pathToPoints(path, maxDist) {
   const points = [[0,10]];
   let x = 0;
   let y = 10;
   const cmds = path.split` `;
   let curCmd;
   const cmdList = 'lLhHvV';
-  //TODO: interpolate between points
+  
   cmds.forEach( cmd => {
     if (cmdList.indexOf(cmd) !== -1) {
       curCmd = cmd;
@@ -48,9 +48,31 @@ function pathToPoints(path) {
     }
   });
 
-  return points;
+  //now expand circlePoints
+  const expandedPoints = [];
+
+  for (let i = 0; i < points.length - 1; i++) {
+    const [cx, cy] = points[i];
+    const [nx, ny] = points[i+1];
+    const d = Math.sqrt((nx - cx) * (nx - cx) + (ny - cy) * (ny - cy));
+    expandedPoints.push(points[i]);
+    if (d > maxDist) {
+      const newSegmentCount = Math.ceil(d / maxDist);
+      const pointDist = d / newSegmentCount;
+      const newPointCount = newSegmentCount - 1;
+      const dx = (nx - cx) / newSegmentCount;
+      const dy = (ny - cy) / newSegmentCount;
+      for (let j = 1; j <= newPointCount; j++) {
+        expandedPoints.push([cx + dx * j, cy + dy * j]);
+      }
+    }
+  }
+  expandedPoints.push(points[points.length-1]);
+
+  return expandedPoints;
+  //return points;
 }
 
 for (let letter in letterPaths) {
-  letterPoints[letter] = pathToPoints(letterPaths[letter]);
+  letterPoints[letter] = pathToPoints(letterPaths[letter], 0.5);
 }
